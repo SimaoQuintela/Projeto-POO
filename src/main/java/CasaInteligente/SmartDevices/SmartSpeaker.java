00e0a847d92d59b102d3afa4976e79a0dad3e366
Goncalo448 <goncalosilva.business@gmail.com>
2022-04-12 13:40:25 +0100

    Progresso na classe SmartSpeaker

--- src/main/java/CasaInteligente/SmartDevices/SmartSpeaker.java
+++ src/main/java/CasaInteligente/SmartDevices/SmartSpeaker.java
@@ -11,43 +11,74 @@ package CasaInteligente.SmartDevices;
  */
 public class SmartSpeaker extends SmartDevice {
     public static final int MAX = 20; //volume máximo
-    
+
     private int volume;
     private String channel;
+    private String brand;
+    private int dailyConsumption; //configurar o consumo nos métodos da classe
 
 
     /**
-     * Constructor for objects of class SmartSpeaker
+     * Construtor por omissão de SmartSpeaker.
      */
     public SmartSpeaker() {
         // initialise instance variables
         super();
         this.channel = "";
         this.volume = 0;
+        this.brand = "";
     }
 
-    public SmartSpeaker(String s, int volume) {
+    /**
+     * Construtor parametrizado de SmartSpeaker.
+     * @param s Canal de rádio.
+     * @param volume Volume da SmartSpeaker.
+     * @param brand Marca da SmartSpeaker.
+     */
+    public SmartSpeaker(String s, int volume, String brand) {
         // initialise instance variables
         super();
         this.channel = s;
         this.volume = volume;
+        this.brand = brand;
     }
 
-    public SmartSpeaker(String cod, String channel, int volume) {
+    /**
+     * Construtor parametrizado de SmartSpeaker.
+     * @param cod Código que identifica a SmartSpeaker.
+     * @param channel Canal de rádio.
+     * @param volume Volume da SmartSpeaker.
+     * @param brand Marca da SmartSpeaker.
+     */
+    public SmartSpeaker(String cod, boolean on, String channel, int volume, String brand) {
         // initialise instance variables
-        super(cod);
-        this.channel = channel;
+        super(cod, on);
+        setChannel(channel);
         this.setVolume(volume);
+        setBrand(brand);
     }
 
+    /**
+     * Construtor de cópia de SmartSpeaker.
+     * @param s SmartSpeaker que é copiada.
+     */
     public SmartSpeaker(SmartSpeaker s){
-        this(s.getID() ,s.getChannel(), s.getVolume());
+        this(s.getID() , s.getOn(), s.getChannel(), s.getVolume(), s.getBrand());
     }
 
+    /**
+     * Método que cria uma cópia de uma SmartSpeaker.
+     * @return Cópia da SmartSpeaker.
+     */
     public SmartSpeaker clone(){
         return new SmartSpeaker(this);
     }
 
+    /**
+     * Método que verifica a igualdade entre a SmartSpeaker e um outro objeto.
+     * @param o objeto comparado com a SmartSpeaker.
+     * @return Booleano que indica o resultado da comparação.
+     */
     public boolean equals(Object o){
         if(this == o)
             return true;
@@ -59,44 +90,81 @@ public class SmartSpeaker extends SmartDevice {
 
         return(
                 this.volume == s.getVolume() &&
-                this.getChannel().equals(s.getChannel())
+                this.getChannel().equals(s.getChannel()) &&
+                this.brand.equals(s.getBrand()));
         );
     }
 
+    /**
+     * Método que produz uma string na qual está representada a SmartSpeaker.
+     * @return String que representa a SmartSpeaker.
+     */
     public String toString(){
         StringBuilder sb = new StringBuilder();
 
         sb.append("------- Smart Speaker -------\n");
         sb.append("Canal: ").append(this.getChannel()).append("\n");
         sb.append("Volume: ").append(this.getVolume()).append("\n");
+        sb.append("Marca: ").append(this.getBrand()).append("\n");
 
         return sb.toString();
     }
 
-
-
-
-
-
-
-
+    /**
+     * Método que aumenta uma unidade no volume da SmartSpeaker.
+     */
     public void volumeUp() {
         if (this.volume<MAX) this.volume++;
     }
 
+    /**
+     * Método que reduz uma unidade no volume da SmartSpeaker.
+     */
     public void volumeDown() {
         if (this.volume>0) this.volume--;
     }
 
+    /**
+     * Método que devolve o volume da SmartSpeaker.
+     * @return Volume da SmartSpeaker.
+     */
     public int getVolume() {return this.volume;}
 
+    /**
+     * Método que altera o volume da SmartSpeaker.
+     * @param i Novo volume da SmartSpeaker.
+     */
     public void setVolume(int i){
         if(i >= 0 && i <= 20 )
             this.volume = i;
     }
 
+    /**
+     * Método que devolve o canal de rádio que está a tocar na SmartSpeaker.
+     * @return Canal de rádio que está a tocar na SmartSpeaker.
+     */
     public String getChannel() {return this.channel;}
-    
+
+    /**
+     * Método que altera o canal de rádio que toca na SmartSpeaker.
+     * @param c Novo canal de rádio que toca na SmartSpeaker.
+     */
     public void setChannel(String c) {this.channel = c;}
+
+    /**
+     * Método que altera a marca da SmartSpeaker.
+     * @param s Nova marca da SmartSpeaker.
+     */
+    public void setBrand(String s){
+        this.brand = s;
+    }
+
+    /**
+     * Método que devolve a marca da SmartSpeaker.
+     * @return Marca da SmartSpeaker.
+     */
+    public String getBrand(){
+        return this.brand;
+    }
 
 }
