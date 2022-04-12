bfb0123619b9dbdddd3584fc300978643208124d
Simao Quintela <simaopedroquintela@hotmail.com>
2022-04-12 16:02:01 +0100

    Changed class name

--- src/main/java/ComercializadoresEnergia/Comercializador.java
+++ src/main/java/ComercializadoresEnergia/Comercializador.java
@@ -1,5 +1,7 @@
 package ComercializadoresEnergia;
+// discutir esta classe com o grupo, não avançar sem debater detalhes da mesma
+public class Comercializador{
 
-public class Mercado {
-    
+
+
 }
