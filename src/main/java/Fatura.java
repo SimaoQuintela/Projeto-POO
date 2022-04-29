a0cc0b85b92675e6d0ff08f244cc766c12546224
Goncalo448 <goncalosilva.business@gmail.com>
2022-04-29 15:35:44 +0100

    Progresso na classe Fatura

--- src/main/java/Fatura.java
+++ src/main/java/Fatura.java
@@ -13,11 +13,12 @@ public class Fatura {
     private String nome;
     private int nif;
     private Map<String, Float> consumoDevice; //id -> consumo
+    private Map<String, SmartDevice> devices; //id -> SmartDevice
     private Comercializador empresa;
     private float total;
 
     /**
-     * Construtor por omissão.
+     * Construtor por omissão de Fatura.
      */
     public Fatura(){
         this.codigo = 0;
@@ -26,15 +27,44 @@ public class Fatura {
         this.consumoDevice = new HashMap<>();
         this.empresa = new Comercializador();
         this.total = 0;
+        this.devices = new HashMap<>();
     }
 
-    public Fatura(int codigo, String nome, int nif, Map<Integer, SmartDevice> consumoDevice, Comercializador empresa, int total){
+    /**
+     * Construtor parametrizado de Fatura.
+     * @param codigo Código da Fatura.
+     * @param nome Nome do cliente.
+     * @param nif Número de identificação fiscal.
+     * @param consumoDevice Id's dos devices associados aos respetivos consumos.
+     * @param empresa Comercializador da Fatura.
+     * @param total Valor final.
+     */
+    public Fatura(int codigo, String nome, int nif, Map<String, Float> consumoDevice, Comercializador empresa, int total){
         setCodigo(codigo);
         setNome(nome);
         setNif(nif);
-
+        setConsumoDevice(consumoDevice);
+        setEmpresa(empresa);
+        valorFinal();
     }
 
+    /**
+     * Construtor de cópia de Fatura.
+     * @param f Fatura que é copiada.
+     */
+    public Fatura(Fatura f){
+        setCodigo(f.getCodigo());
+        setNome(f.getNome());
+        setNif(f.getNif());
+        setConsumoDevice(f.getConsumoDevices());
+        setDevices(f.getDevices());
+        setEmpresa(f.getEmpresa());
+        valorFinal();
+    }
+
+    /**
+     * Método que calcula o valor final da Fatura.
+     */
     public void valorFinal(){
 
     }
@@ -63,13 +93,30 @@ public class Fatura {
         return this.nif;
     }
 
-    public Map<Integer, SmartDevice> getConsumoDevices(){
+    //VERIFICAR ESTE METODO
+    /*public Map<Integer, SmartDevice> getConsumoDevices(){
         Map<String, Float> new_consumos = new HashMap<>();
         new_consumos = this.consumoDevice.entrySet()
                 .stream()
                 .collect(toMap(e->e.getKey(), e->e.getValue()));
 
         return getConsumoDevices();
+    }*/
+
+    /**
+     * Método que devolve o Map com ID's dos SmartDevices associados aos respetivos consumos.
+     * @return Map com ID's dos SmartDevices associados aos respetivos consumos.
+     */
+    public Map<String, Float> getConsumoDevices(){
+        return this.consumoDevice;
+    }
+
+    /**
+     * Método que devolve o Map com ID's dos SmartDevices associados aos respetivos SmartDevices.
+     * @return Map com ID's associados aos respetivos SmartDevices.
+     */
+    public Map<String, SmartDevice> getDevices(){
+        return this.devices;
     }
 
     /**
@@ -107,8 +154,15 @@ public class Fatura {
 
     }
 
-
-
+    /**
+     * Método que altera o Map com os ID's associados aos respetivos SmartDevices.
+     * @param devices
+     */
+    public void setDevices(Map<String, SmartDevice> devices){
+        for(String id: devices.keySet()){
+            this.devices.put(id, devices.get(id).clone());
+        }
+    }
 
     /**
      * Método que altera o Comercializador.
@@ -124,5 +178,33 @@ public class Fatura {
      */
     public Comercializador getEmpresa(){
         return this.empresa;
+    }
+
+    /**
+     * Método que devolve a Fatura em formato de texto.
+     * @return Fatura em formato de texto.
+     */
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Nº da Fatura: ").append(this.codigo).append("\n");
+        sb.append("Nome: ").append(this.nome).append("\n");
+        sb.append("Número de identificação fiscal: ").append(this.nif).append("\n");
+
+        for(String id: this.consumoDevice.keySet()){
+            sb.append(this.devices.get(id).toString()).append(this.consumoDevice.get(id)).append("\n\n");
+        }
+
+        sb.append(this.empresa.toString());
+        sb.append("Valor final: ").append(this.total).append("\n");
+
+        return sb.toString();
+    }
+
+    /**
+     * Método que cria uma cópia da Fatura original.
+     * @return
+     */
+    public Fatura clone(){
+        return new Fatura(this);
     }
 }
