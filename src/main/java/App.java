9b9d80ee6759d4a8f4858c244d6b2b7a71a9f2e8
SimaoQuintela <simaopedroquintela@hotmail.com>
2022-05-03 13:05:08 +0100

    estrutura de dados para o parser montada

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,8 +6,12 @@ import ComercializadoresEnergia.Comercializador;
 
 
 import java.io.FileNotFoundException;
-
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Scanner;
+import java.util.TreeMap;
+
 import static java.lang.System.out;
 
 public class App {
@@ -17,10 +21,25 @@ public class App {
     public static void main(String args[]) throws InterruptedException, FileNotFoundException {
         Comunidade comunidade = new Comunidade("Jackson");
         Parser.parse(comunidade);
+        TreeMap<String, List<List<String>>> actions = new TreeMap<>();
         out.println("---- Inicio dos testes ----");
         out.println(comunidade);
 
+        actions = SimulParser.simulParser();
+        for(String k: actions.keySet()){
+            out.print(k.toString() + " : ");
+            for(List<String> l2 : actions.get(k)){
+                out.print(l2.toString() + " | ");
+            }
+            out.println();
+        }
+
+        // Avançar no tempo
+        LocalDateTime teste = LocalDateTime.now();
+        LocalDateTime novo = LocalDateTime.of(2022, 05, 02, 20, 10).minusMinutes(10); // ano - mes - dia - hora - minuto - segundo
+        out.println(ChronoUnit.HOURS.between(novo, teste));
 
     }
+
 
 }
---
+++ src/main/java/SimulParser.java
@@ -0,0 +1,54 @@
+import static java.lang.System.out;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class SimulParser {
+    public static TreeMap<String, List<List<String>>> simulParser(){
+        List<String> linhas = lerFicheiro("simulacao.txt");
+        List<String[]> linhasSplitted = new ArrayList<>();
+        TreeMap<String, List<List<String>>> actions = new TreeMap<>();
+
+        for(String l: linhas){
+            linhasSplitted.add(l.split(","));
+        //    out.println(Arrays.toString(l.split(",")));
+        }
+
+        String linhaMaisRecente = linhasSplitted.get(0)[0];
+        //out.println(linhaMaisRecente);
+
+
+        for(String[] l: linhasSplitted){
+            linhaMaisRecente = l[0];
+            List<String> modif = new ArrayList<>();
+            for(String s: l) {
+                if (!s.equals(linhaMaisRecente))     // são diferentes
+                    modif.add(s);
+            }
+
+            if(actions.containsKey(l[0])){
+                actions.get(l[0]).add(modif);
+            } else {
+                List<List<String>> novo = new ArrayList<>();
+                actions.put(linhaMaisRecente,novo);
+                actions.get(linhaMaisRecente).add(modif);
+            }
+        }
+
+
+        return actions;
+    }
+
+    public static List<String> lerFicheiro(String nomeFich) {
+        List<String> lines;
+        try { lines = Files.readAllLines(Paths.get(nomeFich), StandardCharsets.UTF_8); }
+        catch(IOException exc) { lines = new ArrayList<>(); }
+        return lines;
+    }
+
+
+
+
+}
